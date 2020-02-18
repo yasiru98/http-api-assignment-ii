524784a91a1adbfe9c0dc54e8b0e31d9c12b93b8
yasiru98 <yasiru043@gmail.com>
2020-02-18 13:42:23 -0500

    done

---
+++ src/server.js
@@ -0,0 +1,65 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    const body = [];
+
+    request.on('error', (err) => {
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+
+      jsonHandler.addUser(request, response, bodyParams);
+    });
+  }
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  switch (request.method) {
+    case 'GET':
+      if (parsedUrl.pathname === '/') {
+        htmlHandler.getIndex(request, response);
+      } else if (parsedUrl.pathname === '/style.css') {
+        htmlHandler.getCSS(request, response);
+      } else if (parsedUrl.pathname === '/getUsers') {
+        jsonHandler.getUsers(request, response);
+      } else {
+        jsonHandler.notReal(request, response);
+      }
+      break;
+    case 'HEAD':
+      if (parsedUrl.pathname === '/getUsers') {
+        jsonHandler.getUsersMeta(request, response);
+      } else {
+        jsonHandler.notRealMeta(request, response);
+      }
+      break;
+    case 'POST':
+      handlePost(request, response, parsedUrl);
+      break;
+    default:
+      jsonHandler.notReal(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
